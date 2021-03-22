231516585222f7aed4d064dd158e4aa288c0c849
Craig McMahon <cmcmahon@sgl.com>
2021-03-22 11:23:44 -0400

    Port state machine in from testbench

--- lab10_1_1/src/hdl/mealy.v
+++ lab10_1_1/src/hdl/mealy.v
@@ -21,6 +21,46 @@
 
 
 module mealy(
-
+    input wire clk,
+    input wire reset,
+    input wire ain,
+    output reg yout,
+    output reg [3:0] count = 4'b1111
     );
+
+// count register
+reg [3:0] next_count;
+// let's count some ones
+always @(posedge clk, posedge reset) begin
+    if (reset) count <= 0;
+    else count <= next_count;
+end
+
+// compute the next value of count
+always @( count, ain ) begin
+    if ( ain ) begin
+        if ( count == 15 ) next_count = 0;
+        else next_count = count + 1;
+    end
+    else begin // ~ain
+        next_count = count;
+    end
+end
+
+// compute the value of yout
+always @(count, ain) begin
+    // current count is divisible by 3; input makes it not
+    if ( count == 3 | count == 6 | count == 9 | count == 12 | count == 15 ) begin
+        yout = ~ain;
+    end
+    // for (n+1)%3=0; adding a one makes it divisible by 3
+    else if ( count == 2 | count == 5 | count == 8 | count == 13 | count == 14 ) begin
+        yout = ain;
+    end
+    // for 0, 1, 4, ... it doesn't matter; the input isn't enough for %3=0
+    else begin
+        yout = 0;
+    end
+end
+
 endmodule
