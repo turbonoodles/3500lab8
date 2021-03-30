c3801fb620b9a589a09bf9374f84e694d0cb755f
Craig McMahon <cmcmahon@sgl.com>
2021-03-30 11:54:23 -0400

    this doesn't work either

--- lab10_2_1/src/sim/moore_tb.v
+++ lab10_2_1/src/sim/moore_tb.v
@@ -33,11 +33,13 @@ reg yout = 0;
 parameter [3:0] R = 4'b0000; // reset state
 parameter [3:0] G01 = 1, G0100 = 4;
 parameter [3:0] G11 = 3, G1100 = 5;
-parameter [3:0] G10 = 2, G1000 = 6;
+parameter [3:0] G10 = 4'b0010;
+parameter [3:0] G1000H = 4'b0110;
+parameter [3:0] G1000L = 4'b1110;
 parameter [3:0] H = 7; // hold state
 
-reg [2:0] state = R;
-reg [2:0] next_state;
+reg [3:0] state = R;
+reg [3:0] next_state;
 // drive state machine
 always @( posedge clk, posedge reset ) begin
     if (reset) state <= R;
@@ -45,7 +47,7 @@ always @( posedge clk, posedge reset ) begin
 end
 
 // calculate next state
-always @( state, ain ) begin
+always @( state, ain, yout ) begin
     #1;
     case ( state )
         R: begin
@@ -75,7 +77,10 @@ always @( state, ain ) begin
         end
         G10: begin
             case (ain)
-                2'b00: next_state = G1000;
+                2'b00: begin
+                    if ( yout ) next_state = G1000H;
+                    else next_state = G1000L;
+                end
                 2'b01: next_state = G01;
                 2'b10: next_state = G10;
                 2'b11: next_state = G11;
@@ -100,7 +105,16 @@ always @( state, ain ) begin
                 default: next_state = H; 
             endcase
         end
-        G1000: begin
+        G1000L: begin
+            case (ain)
+                2'b00: next_state = H;
+                2'b01: next_state = G01;
+                2'b10: next_state = G10;
+                2'b11: next_state = G11;
+                default: next_state = H; 
+            endcase
+        end 
+        G1000H: begin
             case (ain)
                 2'b00: next_state = H;
                 2'b01: next_state = G01;
@@ -122,12 +136,17 @@ always @( state, ain ) begin
     endcase
 end
 
+reg yout_prev;
 // calculate outputs
 always @( state ) begin
-    if ( state == R ) yout = 0;
-    else if ( state == G1100 ) yout = 1; // synchronous set
+    yout_prev = yout;
+    yout = 0;
+    if ( state == G1100 ) yout = 1; // synchronous set
     else if ( state == G0100 ) yout = 0; // synchronous clear
-    else if ( state == G1000 ) yout = ~yout; // toggle
+    else if ( state == G1000H ) yout = 0; // toggle
+    else if ( state == G1000L ) yout = 1; // toggle
+    else if ( state == H ) yout = yout_prev;
+    else yout = 0;
 end 
 
 //testbench stuff
